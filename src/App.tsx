679fa1596136ac0a47ce2478315281d6eaebf076
mulithemuli <philipp.lohmeyer@gmail.com>
2023-02-17 16:07:29 +0100

    changing style to scss and providing dark-mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,68 @@
-import React from 'react';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import './App.scss';
 import CommentsComponent from "./component/CommentsComponent";
+import {createTheme, CssBaseline, IconButton, ThemeProvider} from "@mui/material";
+import {WbSunny} from "@mui/icons-material";
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark'
+    }
+});
+
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light'
+  }
+});
 
 function App() {
+  const [chosenTheme, setChosenTheme] = useState(lightTheme);
+  if (chosenTheme.palette.mode === 'light') {
+
+  }
+  useEffect(() => {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setChosenTheme(darkTheme);
+    }
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+      setChosenTheme(event.matches ? darkTheme : lightTheme);
+    });
+  });
+
+  const readModeSwitchLabel = () => {
+    if (chosenTheme.palette.mode === 'light') {
+      return 'Auf Nachtmodus wechseln';
+    } else {
+      return 'Auf Tagmodus wechseln';
+    }
+  }
+
+  const switchTheme = () => {
+    if (chosenTheme.palette.mode === 'light') {
+      setChosenTheme(darkTheme);
+    } else {
+      setChosenTheme(lightTheme);
+    }
+  }
+
   return (
-    <div className="App">
+    <ThemeProvider theme={chosenTheme}>
+      <CssBaseline/>
       <header className="App-header">
-        <h1>React Comment Demo App</h1>
+        <div className="body-container">
+          <h1>React Comment Demo App</h1>
+          <nav>
+            <IconButton aria-label={readModeSwitchLabel()} onClick={switchTheme}>
+              <WbSunny/>
+            </IconButton>
+          </nav>
+        </div>
       </header>
-      <CommentsComponent/>
-    </div>
+      <main className="body-container">
+        <CommentsComponent/>
+      </main>
+    </ThemeProvider>
   );
 }
 
